4e097d9584c5b6e08e050f8e12993add580ef24f
Geoff Brodniak <geoff.brodniak@teamsnap.com>
2014-02-12 15:01:59 -0600

    More fixes related to namespace changes

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from issue_tracker import views
 
-urlpatterns = patterns('collaborate.issue_tracker.views',
+urlpatterns = patterns('issue_tracker.views',
     url(r'^issue_managers/', views.IssueManagerListView.as_view()),
     url(r'^issue_managers/(?P<pk>[0-9]+)/', views.IssueManagerDetailView.as_view()),
     url(r'^issues/(?P<pk>[0-9]+)/', views.IssueListView.as_view()),
